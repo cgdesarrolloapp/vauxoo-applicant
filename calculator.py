b3e31700b66b6b084bdf5321851723cf61f95fc3
cgdesarrolloapp <cgdesarrolloapp@gmail.com>
2017-07-15 16:48:19 -0400

    py1 y py2

--- calculator.py
+++ calculator.py
@@ -1,16 +1,22 @@
 """
-Your module documentation here
+CalculatorClass gets a number list
+and return the sum of each one of them
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    With a for cicle the num_list retreive each one
+    of the elements wich is sum with the acum variable.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        num_list is the list of number wich will be sum
+        number variant will have each value of the num_list numbers
+        suma will sum all the values
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        suma = 0
+        for number in num_list:
+            suma = number + suma
+        return suma
--- primes.py
+++ primes.py
@@ -1,16 +1,30 @@
 """
-Your module documentation here
+PrimeClass with method is_prime indicate when integer number is prime number
 """
+import math
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    is_prime method will return false or true when the integer
+    number is primer or not
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        First validate numbers inder 2 and equals 2, later iterates from 2
+        until num_int-1 with temp variant
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        # Start evaluation
+        if num_int < 2:
+            return False
+        elif num_int == 2:
+            return True
+        else:
+            temp = math.floor(num_int/2)
+            while temp >= 2:
+                if num_int % temp == 0:
+                    return False
+                else:
+                    temp = temp-1
+            return True
