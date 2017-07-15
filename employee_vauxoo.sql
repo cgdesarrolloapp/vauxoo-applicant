cd5c9b8769e67f5409700d186869b73ad594104d
cgdesarrolloapp <cgdesarrolloapp@gmail.com>
2017-07-15 10:32:33 -0400

    SQL2 creacion de tabla employee_hobby y asignacion de hobby a cada empleado

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -5,19 +5,33 @@
 --       La prueba Unitest de SQL indica error en la cantidad de campos creados en tabla employee el campo adicional es necesario para hacer la relacion entre employee y employee_department.
 
 CREATE TABLE employee_department (
-	id   int              not null,
-	name varchar (50)      not null,
-	description varchar (100)      not null,
+	id   		integer		NOT NULL,
+	name 		varchar (50)	NOT NULL,
+	description 	varchar (100)	NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-	id   int              not null,
-	first_name varchar (50)      not null,
-	last_name varchar (50)      not null,
-	deparment_id   int       not null,
+	id   		integer       NOT NULL,
+	first_name 	varchar (50)  NOT NULL,
+	last_name 	varchar (50)  NOT NULL,
+	deparment_id   	integer       NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (deparment_id) REFERENCES employee_department(id) 
+);
+
+CREATE TABLE employee_hobby (
+	id   		integer 	NOT NULL,
+	name 		varchar (50) 	NOT NULL,
+	description	varchar(100)	NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_employee_hobby (
+	employee_id    integer NOT NULL,
+	hobby_id       integer NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employee(id),
+	FOREIGN KEY (hobby_id) REFERENCES employee_hobby(id)
 );
 
 INSERT INTO employee_department VALUES (1, 'Administracion','Departamento de administracion y finanzas');
@@ -32,3 +46,15 @@ INSERT INTO employee VALUES (2, 'Marisol','Perez', 2);
 INSERT INTO employee VALUES (3, 'Jose','Paez', 5);
 INSERT INTO employee VALUES (4, 'Margarita','Ruiz', 4);
 
+INSERT INTO employee_hobby VALUES (1, 'Natacion', 'Deporte acuatico');
+INSERT INTO employee_hobby VALUES (2, 'Pintura', 'Pinta cuadros');
+INSERT INTO employee_hobby VALUES (3, 'Filatelia', 'Colecciona sellos y estampillas');
+
+INSERT INTO employee_employee_hobby VALUES (1, 1);
+INSERT INTO employee_employee_hobby VALUES (1, 3);
+INSERT INTO employee_employee_hobby VALUES (2, 3);
+INSERT INTO employee_employee_hobby VALUES (2, 2);
+INSERT INTO employee_employee_hobby VALUES (3, 1);
+INSERT INTO employee_employee_hobby VALUES (3, 2);
+INSERT INTO employee_employee_hobby VALUES (4, 3);
+INSERT INTO employee_employee_hobby VALUES (4, 2);
