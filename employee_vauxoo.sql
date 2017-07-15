54de3be0915a55d9b21625588c0e2c85ddac48f9
cgdesarrolloapp <cgdesarrolloapp@gmail.com>
2017-07-15 12:32:24 -0400

    SQL3 Relacion jefe empleado

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -5,33 +5,36 @@
 --       La prueba Unitest de SQL indica error en la cantidad de campos creados en tabla employee el campo adicional es necesario para hacer la relacion entre employee y employee_department.
 
 CREATE TABLE employee_department (
-	id   		integer		NOT NULL,
-	name 		varchar (50)	NOT NULL,
-	description 	varchar (100)	NOT NULL,
-	PRIMARY KEY (id)
+    id           integer        NOT NULL,
+    name         varchar (50)    NOT NULL,
+    description     varchar (100)    NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-	id   		integer       NOT NULL,
-	first_name 	varchar (50)  NOT NULL,
-	last_name 	varchar (50)  NOT NULL,
-	deparment_id   	integer       NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (deparment_id) REFERENCES employee_department(id) 
+    id           integer       NOT NULL,
+    first_name     varchar (50)  NOT NULL,
+    last_name     varchar (50)  NOT NULL,
+    deparment_id       integer       NOT NULL,
+    manager_id       integer       NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT manager_id FOREIGN KEY (manager_id) REFERENCES employee_employee.employee(id),
+    FOREIGN KEY (deparment_id) REFERENCES employee_employee.employee_department (id)
 );
 
 CREATE TABLE employee_hobby (
-	id   		integer 	NOT NULL,
-	name 		varchar (50) 	NOT NULL,
-	description	varchar(100)	NOT NULL,
-	PRIMARY KEY (id)
+    id           integer     NOT NULL,
+    name         varchar (50)     NOT NULL,
+    description    varchar(100)    NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE employee_employee_hobby (
-	employee_id    integer NOT NULL,
-	hobby_id       integer NOT NULL,
-	FOREIGN KEY (employee_id) REFERENCES employee(id),
-	FOREIGN KEY (hobby_id) REFERENCES employee_hobby(id)
+CREATE TABLE rel_employee_employee_hobby (
+    employee_id            integer ,
+    employee_hobby_id       integer ,
+    PRIMARY KEY (employee_id, employee_hobby_id),
+    FOREIGN KEY (employee_id) REFERENCES employee_employee.employee (id),
+    FOREIGN KEY (employee_hobby_id) REFERENCES employee_employee.employee_hobby (id)
 );
 
 INSERT INTO employee_department VALUES (1, 'Administracion','Departamento de administracion y finanzas');
@@ -41,20 +44,20 @@ INSERT INTO employee_department VALUES (4, 'Logistica','Departamento de Logistic
 INSERT INTO employee_department VALUES (5, 'RRHH','Departamento de Recursos Humanos');
 INSERT INTO employee_department VALUES (6, 'Compras','Departamento de Compras');
 
-INSERT INTO employee VALUES (1, 'Fernando','Diaz', 2);
-INSERT INTO employee VALUES (2, 'Marisol','Perez', 2);
-INSERT INTO employee VALUES (3, 'Jose','Paez', 5);
-INSERT INTO employee VALUES (4, 'Margarita','Ruiz', 4);
+INSERT INTO employee VALUES (1, 'Fernando','Diaz', 2, null );
+INSERT INTO employee VALUES (2, 'Marisol','Perez', 2, 1);
+INSERT INTO employee VALUES (3, 'Jose','Paez', 5, 1);
+INSERT INTO employee VALUES (4, 'Margarita','Ruiz', 4, 1);
 
 INSERT INTO employee_hobby VALUES (1, 'Natacion', 'Deporte acuatico');
 INSERT INTO employee_hobby VALUES (2, 'Pintura', 'Pinta cuadros');
 INSERT INTO employee_hobby VALUES (3, 'Filatelia', 'Colecciona sellos y estampillas');
 
-INSERT INTO employee_employee_hobby VALUES (1, 1);
-INSERT INTO employee_employee_hobby VALUES (1, 3);
-INSERT INTO employee_employee_hobby VALUES (2, 3);
-INSERT INTO employee_employee_hobby VALUES (2, 2);
-INSERT INTO employee_employee_hobby VALUES (3, 1);
-INSERT INTO employee_employee_hobby VALUES (3, 2);
-INSERT INTO employee_employee_hobby VALUES (4, 3);
-INSERT INTO employee_employee_hobby VALUES (4, 2);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (1, 1);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (1, 3);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (2, 3);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (2, 2);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (3, 1);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (3, 2);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (4, 3);
+INSERT INTO rel_employee_employee_hobby ( employee_id , employee_hobby_id ) VALUES (4, 2);
