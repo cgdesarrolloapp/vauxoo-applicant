9f108b54fae2002d1061678eba4325bf4143fb8a
cgdesarrolloapp <cgdesarrolloapp@gmail.com>
2017-07-16 23:56:13 -0400

    travis 4

--- primes.py
+++ primes.py
@@ -1,28 +1,29 @@
 """
-PrimeClass with method is_prime checks when integer number is prime number
+PrimeClass with method is_prime indicate when integer number is prime number
 """
 import math
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    is_prime method will return false or true when the integer
+    number is primer or not
     """
 
     def is_prime(self, num_int):
         """
-        First validate numbers over 2 and equals 2, later iterates from 2
-        until num_int-1 with aux variant
+        First validate numbers inder 2 and equals 2, later iterates from 2
+        until num_int-1 with temp variant
         """
+        # Start evaluation
         if num_int < 2:
             return False
         elif num_int == 2:
             return True
-        else:
-            aux = math.floor(num_int/2)
-            while aux >= 2:
-                if num_int % aux == 0:
-                    return False
-                else:
-                    aux = aux-1
-            return True
+        temp = math.floor(num_int/2)
+        while temp >= 2:
+            if num_int % temp == 0:
+                return False
+            else:
+                temp = temp-1
+        return True
