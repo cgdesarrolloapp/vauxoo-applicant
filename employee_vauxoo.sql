62d4ea9aba156cf7eab3b5d4f2f60ff5fc976ebf
cgdesarrolloapp <cgdesarrolloapp@gmail.com>
2017-07-16 21:24:26 -0400

    sql2 hobby employee

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -16,6 +16,20 @@ CREATE TABLE employee (
 
 );
 
+CREATE TABLE employee_hobby (
+	id          serial,
+	name        varchar (50) NOT NULL,
+	description varchar (50),
+	CONSTRAINT  employee_hobby_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_employee_hobby (
+	employee_id       serial,
+	employee_hobby_id integer NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employee (id),
+	FOREIGN KEY (employee_hobby_id) REFERENCES employee_hobby (id)
+);
+
 
 INSERT INTO employee_department (name, description) VALUES ('IT', 'Informacin y tecnologia');
 INSERT INTO employee_department (name, description) VALUES ('RRHH', 'Recursos Humanos');
@@ -28,3 +42,16 @@ INSERT INTO employee (first_name, last_name, employee_department_id ) VALUES ('M
 INSERT INTO employee (first_name, last_name, employee_department_id ) VALUES ('Juan', 'Perez','2');
 INSERT INTO employee (first_name, last_name, employee_department_id ) VALUES ('Carlos', 'Ramirez','4');
 INSERT INTO employee (first_name, last_name, employee_department_id ) VALUES ('Luis', 'Diaz','4');
+
+INSERT INTO employee_hobby (name, description) VALUES ('Natacion', 'practica natacion');
+INSERT INTO employee_hobby (name, description) VALUES ('Arte', 'Artista');
+INSERT INTO employee_hobby (name, description) VALUES ('Viajes', 'Prectica turismo');
+
+INSERT INTO employee_employee_hobby VALUES (1, 2);
+INSERT INTO employee_employee_hobby VALUES (1, 1);
+INSERT INTO employee_employee_hobby VALUES (2, 3);
+INSERT INTO employee_employee_hobby VALUES (2, 1);
+INSERT INTO employee_employee_hobby VALUES (3, 1);
+INSERT INTO employee_employee_hobby VALUES (3, 2);
+INSERT INTO employee_employee_hobby VALUES (4, 1);
+INSERT INTO employee_employee_hobby VALUES (4, 3);
